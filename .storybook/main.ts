6f0e668c9df2d925103772b3c55566a890d727b4
nesthcher <iyariki@bk.ru>
2025-03-16 23:23:17 +0300

    del: Удаление аддона для Storybook
    Удаление аддона для Storybook (@storybook/addon-onboarding)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
 	addons: [
-		"@storybook/addon-onboarding",
 		"@storybook/addon-essentials",
 		"@storybook/addon-a11y",
 		"@chromatic-com/storybook",
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,7 @@ import "../src/assets/styles/resetStyles.css";
 import "../src/assets/styles/global.css";
 
 import type { Preview } from "@storybook/react";
+
 const preview: Preview = {
 	parameters: {
 		controls: {
